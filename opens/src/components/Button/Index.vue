bbe120a44c05b9b5d0ed7f438fb8571ed069c393
thaislorentzz <thais@hivesys.com.br>
2021-11-19 22:19:31 -0300

    button component

---
+++ opens/src/components/Button/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="evt => $emit('click', evt)">{{text}}o</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/_buttons.scss";
+</style>
