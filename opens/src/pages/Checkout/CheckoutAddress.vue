5cd43ed996115f73e951d0e44483d87994ec9b9f
thaislorentzz <thais@hivesys.com.br>
2021-11-23 12:48:57 -0300

    viacep api checkout address

--- opens/src/pages/Checkout/CheckoutAddress.vue
+++ opens/src/pages/Checkout/CheckoutAddress.vue
@@ -1,60 +1,59 @@
 <template>
   <div class="checkout">
     <div class="checkout-container">
-
-    <div class="checkout-header">
-      <router-link to="/">
-        <img
-          class="checkout-image"
-          alt="Hamburger logo"
-          src="../../assets/images/logo.png"
+      <div class="checkout-header">
+        <router-link to="/">
+          <img
+            class="checkout-image"
+            alt="Hamburger logo"
+            src="../../assets/images/logo.png"
+          />
+        </router-link>
+        <h1 class="checkout-title">CHECKOUT</h1>
+      </div>
+      <div class="checkout-icones">
+        <font-awesome-icon
+          class="checkout-hamburguer select"
+          :icon="['fab', 'font-awesome']"
         />
-      </router-link>
-      <h1 class="checkout-title">CHECKOUT</h1>
-    </div>
-    <div class="checkout-icones">
-      <font-awesome-icon
-        class="checkout-hamburguer select"
-        :icon="['fab', 'font-awesome']"
-      />
-      <font-awesome-icon
-        class="checkout-hamburguer select"
-        :icon="['fab', 'font-awesome']"
-      />
-      <font-awesome-icon
-        class="checkout-hamburguer select"
-        :icon="['fab', 'font-awesome']"
-      />
-    </div>
-    <div>
-      <h1 class="checkout-text">Onde iremos realizar a entregar?</h1>
-    </div>
-    <div class="checkout-form">
-      <div class="checkout-address">
-        <div class="checkout-address-input">
-          <Input type="form" />
-        </div>
-        <div class="checkout-address-input size">
-          <Input type="form" />
-        </div>
-        <div class="checkout-address-input">
-          <Input type="form" />
-        </div>
-        <div class="checkout-address-input">
-          <Input type="form" />
-        </div>
-        <div class="checkout-address-input">
-          <Input type="form" />
-        </div>
-        <div class="checkout-address-input size">
-          <Input type="form" />
+        <font-awesome-icon
+          class="checkout-hamburguer select"
+          :icon="['fab', 'font-awesome']"
+        />
+        <font-awesome-icon
+          class="checkout-hamburguer select"
+          :icon="['fab', 'font-awesome']"
+        />
+      </div>
+      <div>
+        <h1 class="checkout-text">Onde iremos realizar a entregar?</h1>
+      </div>
+      <div class="checkout-form">
+        <div class="checkout-address">
+          <div class="checkout-address-input">
+            <Input type="form" placeholder="CEP" v-model="postalCode" />
+          </div>
+          <div class="checkout-address-input size">
+            <Input type="form" placeholder="Logradouro" v-model="street" />
+          </div>
+          <div class="checkout-address-input">
+            <Input type="form" placeholder="Número" v-model="number" />
+          </div>
+          <div class="checkout-address-input">
+            <Input type="form" placeholder="Bairro" v-model="neighborhood" />
+          </div>
+          <div class="checkout-address-input">
+            <Input type="form" placeholder="Cidade" v-model="city" />
+          </div>
+          <div class="checkout-address-input size">
+            <Input type="form" placeholder="Complemento" v-model="complement" />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="checkout-button">
-      <Button @click="() => this.$router.push('/checkout-2')" />
-      <Button @click="() => this.$router.push('/finalize')" />
-    </div>
+      <div class="checkout-button">
+        <Button @click="() => this.$router.push('/checkout-2')" />
+        <Button @click="() => this.$router.push('/finalize')" />
+      </div>
     </div>
     <Footer />
   </div>
@@ -64,6 +63,7 @@
 import Button from "../../components/Button/Index.vue";
 import Input from "../../components/Input/Index.vue";
 import Footer from "../../components/Footer/Index.vue";
+import axios from "axios";
 
 export default {
   name: "CheckoutContact",
@@ -74,6 +74,40 @@ export default {
     Button,
     Input,
     Footer,
+  },
+  data() {
+    return {
+      postalCode: "",
+      street: "",
+      number: "",
+      complement: "",
+      neighborhood: "",
+      city: "",
+    };
+  },
+  watch: {
+    postalCode: function () {
+      console.log(this.postalCode);
+      this.searchCep(this.postalCode);
+    },
+  },
+  methods: {
+    searchCep(cep) {
+      if (cep.length == 8) {
+        
+        console.log(cep);
+        axios
+          .get(`https://viacep.com.br/ws/${cep}/json/`)
+          .then((response) => {
+            console.log(response.data)
+            this.street = response.data.logradouro;
+            this.neighborhood = response.data.bairro;
+            this.city = response.data.localidade;
+            this.complement = response.data.complemento;
+          })
+          .catch((error) => console.log(error));
+      }
+    },
   },
 };
 </script>
@@ -93,7 +127,6 @@ export default {
       &.size {
         width: 600px;
       }
-
     }
   }
 }
@@ -101,13 +134,12 @@ export default {
 @media screen and (max-width: 600px) {
   .checkout {
     &-address {
-    &-input {
-      &.size {
-        width: 300px;
+      &-input {
+        &.size {
+          width: 300px;
+        }
       }
-
     }
-  }
   }
 }
 </style>
