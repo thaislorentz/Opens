21cf7afd5210a55cc3cb83b871e80d9cd72261ed
thaislorentzz <thais@hivesys.com.br>
2021-11-22 23:18:28 -0300

    fix component bag

--- opens/src/components/Bag/Index.vue
+++ opens/src/components/Bag/Index.vue
@@ -1,59 +1,43 @@
 <template>
   <div class="bag">
     <h2>Pedido</h2>
-    <div class="bag-line"/>
-    <div
-      v-for="(promotion, index) in promotions"
-      :key="index"
-      class="bag-items"
-    >
-      <img class="bag-image" :src="promotion.img" :alt="promotion.name" />
+    <div class="bag-line" />
+    <div v-if="selected.lenght > 0">
+    <div v-for="(food, index) in selected" :key="index" class="bag-items" >
+      <img class="bag-image" :src="food.img" :alt="food.name" />
       <div>
-        <h4 class="bag-items-name">{{ promotion.name }}</h4>
-        <p class="bag-items-description">{{ promotion.description }}</p>
+        <h4 class="bag-items-name">{{ food.name }}</h4>
+        <p class="bag-items-description">{{ food.description }}</p>
       </div>
       <div class="bag-delete">
-      <font-awesome-icon class="social-icon" :icon="['fab', 'font-awesome']" />
+        <font-awesome-icon
+          class="social-icon"
+          :icon="['fab', 'font-awesome']"
+        />
       </div>
     </div>
-    <div class="bag-button">
-      <router-link to="/checkout-1">
-    <Button />
-    </router-link>
     </div>
+    <div class="bag-button" v-if="selected.lenght > 0">
+      <router-link to="/checkout-1">
+        <Button />
+      </router-link>
+    </div>
+    <h2>Não há pedido</h2>
   </div>
 </template>
 
 <script>
-import Button from '../Button/Index.vue'
+import Button from "../Button/Index.vue";
 export default {
   name: "bag",
   components: {
-    Button
+    Button,
+  },
+  props: {
+    selected: Array,
   },
   data() {
-    return {
-      promotions: [
-        {
-          img: "https://www.freeiconspng.com/thumbs/fast-food-png/fast-food-png-most-popular-fast-food-snacks-in-your-area-and-most--3.png",
-          name: "Combo sanduiches",
-          description: "Contém 2 sanduiches, anéis de cebola, batata ....",
-          id: 1,
-        },
-        {
-          img: "https://www.freeiconspng.com/thumbs/fast-food-png/fast-food-png-most-popular-fast-food-snacks-in-your-area-and-most--3.png",
-          name: "Combo sanduiches",
-          description: "Contém 2 sanduiches, anéis de cebola, batata ....",
-          id: 2,
-        },
-        {
-          img: "https://www.freeiconspng.com/thumbs/fast-food-png/fast-food-png-most-popular-fast-food-snacks-in-your-area-and-most--3.png",
-          name: "Combo sanduiches",
-          description: "Contém 2 sanduiches, anéis de cebola, batata ....",
-          id: 3,
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
@@ -62,16 +46,16 @@ export default {
 @import "@/assets/scss/_colors.scss";
 h2 {
   text-align: center;
-    color: $predominant;
-  margin: 10px
+  color: $predominant;
+  margin: 10px;
 }
 .bag {
   background: $white;
   width: 30%;
   &-line {
-  width: 100%;
-  background: $secondary;
-  height: 1px;
+    width: 100%;
+    background: $secondary;
+    height: 1px;
   }
   &-image {
     width: 80px;
@@ -100,20 +84,20 @@ h2 {
 }
 
 @media screen and (max-width: 1000px) {
-.bag {
-  width: 50%;
-}
-}
-@media screen and (max-width: 800px) {
-.bag {
-  width: 100%;
-  padding: 10px;
-  &-items {
-    justify-content: center;
-    width: 100%;
-    margin: 0;
-    margin-bottom: 10px;
+  .bag {
+    width: 50%;
   }
 }
+@media screen and (max-width: 800px) {
+  .bag {
+    width: 100%;
+    padding: 10px;
+    &-items {
+      justify-content: center;
+      width: 100%;
+      margin: 0;
+      margin-bottom: 10px;
+    }
+  }
 }
 </style>
